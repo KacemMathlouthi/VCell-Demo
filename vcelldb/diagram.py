18a0518f9a36643d6c63f4e96da2dbae53b0324e
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-04-05 17:25:10 +0100

    Feat: Function to get biomodel keys from api response

--- vcelldb/diagram.py
+++ vcelldb/diagram.py
@@ -1,3 +1,22 @@
+def get_bmkeys(api_response: dict):
+    """
+    Retrieves the BioModel keys from the API response.
+
+    Args:
+        api_response (dict): The response from the VCell API, contains a list of models.
+
+    Returns:
+        List[str]: A list of BioModel keys.
+    """
+    bmkeys = []
+
+    for model in api_response:
+        biomodel_id = model.get("bmKey")
+        if biomodel_id:
+            bmkeys.append(biomodel_id)
+    return bmkeys
+
+
 def get_diagram_urls(api_response: dict):
     """
     Retrieves the diagram URLs of VCell models from the API response.
@@ -9,10 +28,12 @@ def get_diagram_urls(api_response: dict):
         List[str]: A list of diagram URLs.
     """
     urls = []
+    bmkeys = get_bmkeys(api_response)
+    if not bmkeys:
+        return urls
 
-    for model in api_response:
-        biomodel_id = model.get("bmKey")
-        if biomodel_id:
-            diagram_url = f"https://vcell.cam.uchc.edu/api/v0/biomodel/{biomodel_id}/diagram"
+    for bmkey in bmkeys:
+        if bmkey:
+            diagram_url = f"https://vcell.cam.uchc.edu/api/v0/biomodel/{bmkey}/diagram"
             urls.append(diagram_url)
     return urls
